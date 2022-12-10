6fd71c2ce7bc2227009ad8b82d0fb3c1c81b6c6f
Chris Kirkland <chriskirkland@github.com>
2022-12-10 00:28:40 -0600

    2022-10 ruby

---
+++ 2022/10/sol.rb
@@ -0,0 +1,23 @@
+# https://adventofcode.com/2022/day/10
+$data = File.read("input.txt").split("\n").map{ _1.split(" ") }
+
+# generate values of x register at each clock cycle
+x = 1
+xvals = $data.map { |instr|
+    next x if instr[0] == "noop"
+
+    v = [x,x]
+    x += instr[1].to_i
+    v
+}.insert(x).flatten
+
+# part 1
+CYCLES = [20, 60, 100, 140, 180, 220]
+puts CYCLES.map { |c| c * xvals[c-1] }.reduce(:+) 
+
+# part 2
+pixels = xvals.each_with_index.map { |v, c|
+    next "#" if (c%40-v).abs <= 1
+    "."
+}
+puts pixels.each_slice(40).map(&:join).join("\n")
