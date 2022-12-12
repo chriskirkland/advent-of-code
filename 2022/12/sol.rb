26b9f35fd8c2d67d62c9a429b843d1a83dbf11cf
Chris Kirkland <chriskirkland@github.com>
2022-12-12 16:06:06 -0600

    more tidy

--- 2022/12/sol.rb
+++ 2022/12/sol.rb
@@ -1,7 +1,9 @@
 # https://adventofcode.com/2022/day/12
+$input = File.read("input.txt").split("\n").map(&:chars)
 $heights = ('a'..'z').zip(26.downto(1)).to_h.merge({'S' => 26, 'E' => 1})
 
-$input = File.read("input.txt").split("\n").map(&:chars)
+N = $input.count
+M = $input[0].count
 
 def find_all(*chs)
     $input.each_with_index.map { |row, i|
@@ -14,9 +16,7 @@ end
 def solve(*goals)
     grid = $input.map{ |row| row.map{ $heights[_1]}}
 
-    n = $input.count
-    m = $input[0].count
-    dist = Array.new(n) { Array.new(m, n*m) }
+    dist = Array.new(N) { Array.new(M, N*M) }
 
     sx, sy = find_all('E').first
     dist[sx][sy] = 0
@@ -25,16 +25,16 @@ def solve(*goals)
         x, y = queue.shift
 
         [[x-1, y], [x+1, y], [x, y-1], [x, y+1]].each do |nx, ny|
-            next unless nx >= 0 && nx <= n-1 && ny >= 0 && ny <= m-1 # boundary checks
+            next unless nx >= 0 && nx <= N-1 && ny >= 0 && ny <= M-1 # boundary checks
             next unless grid[nx][ny] <= grid[x][y] + 1 # can only climb 1 unit
-            next unless dist[x][y]+1 < dist[nx][ny] # this path is shorter than previous paths
+            next unless dist[x][y]+1 < dist[nx][ny] # proposed path is shorter than previous paths
 
             dist[nx][ny] = dist[x][y]+1 
             queue.push [nx, ny]
         end
     end
 
-    find_all(*goals).map{ |x, y| dist[x][y] }.min
+    find_all(*goals).map { |gx, gy| dist[gx][gy] }.min
 end
 
 puts solve('S')
