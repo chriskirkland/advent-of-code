3cc459561e7bd418b1b24beb5ab3b34dfa9a5b4c
Chris Kirkland <chriskirkland@github.com>
2022-12-12 13:46:59 -0600

    2022-12 p2

--- 2022/12/sol.rb
+++ 2022/12/sol.rb
@@ -1,45 +1,33 @@
 # https://adventofcode.com/2022/day/12
-$heights = ('a'..'z').zip(1..26).to_h.merge({'S' => 1, 'E' => 26})
+$heights = ('a'..'z').zip(26.downto(1)).to_h.merge({'S' => 26, 'E' => 1})
 
 $input = File.read("input.txt").split("\n").map(&:chars)
 
-def find_all(ch)
-    results = []
-    $input.each_with_index do |row, i|
-        row.each_with_index do |col, j|
-            if $input[i][j] == ch
-                results.push [i, j]
-            end
-        end
-    end
-    results
+def find_all(*chs)
+    $input.each_with_index.map { |row, i|
+        row.each_with_index.map { |col, j|
+            [i, j] if chs.include? $input[i][j]
+        }
+    }.flatten(1).compact
 end
 
-def solve
+def solve(*goals)
     grid = $input.map{ |row| row.map{ $heights[_1]}}
 
     n = $input.count
     m = $input[0].count
     dist = Array.new(n) { Array.new(m, n*m) }
 
-    #grid.each do |row|
-    #    puts "#{row}"
-    #end
-
-    # TODO: turn this into a heap?
-    sx, sy = find_all('S').first
+    sx, sy = find_all('E').first
     dist[sx][sy] = 0
     queue = [[sx, sy]]
     while queue.count > 0
         x, y = queue.shift
-        #puts "([#{x},#{y}]=#{grid[x][y]}) searching..."
 
         is_candidate = ->(nx, ny) {
-            #puts "([#{x},#{y}]=#{grid[x][y]},d#{dist[x][y]}) checking ([#{nx},#{ny}]=#{grid[nx][ny]}),d#{dist[nx][ny]}"
             grid[nx][ny] <= grid[x][y] + 1 && dist[x][y]+1 < dist[nx][ny]
         }
         visit = ->(nx, ny) {
-            #puts "([#{x},#{y}]=#{grid[x][y]},d#{dist[x][y]}) enqueueing [#{nx},#{ny}]"
             dist[nx][ny] = dist[x][y]+1 
             queue.push [nx, ny]
         }
@@ -49,8 +37,9 @@ def solve
         visit.call(x, y-1) if y > 0 && is_candidate.call(x, y-1)
         visit.call(x, y+1) if y < m-1 && is_candidate.call(x, y+1)
     end
-    ex, ey = find_all('E').first
-    dist[ex][ey]
+
+    find_all(*goals).map{ |x, y| dist[x][y] }.min
 end
 
-puts solve()
+puts solve('S')
+puts solve('S', 'a')
