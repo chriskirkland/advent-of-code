257e488e001f9f211319da43fbc00e196f257e7a
Chris Kirkland <chriskirkland@github.com>
2022-12-23 10:16:06 -0600

    tidy & optimize

--- 2022/23/sol.rb
+++ 2022/23/sol.rb
@@ -1,10 +1,5 @@
 # https://adventofcode.com/2022/day/23
 $data = File.read("input.txt").split("\n")
-DEBUG = false
-
-def debug(msg)
-    puts msg if DEBUG
-end
 
 def vadd(v1, v2)
     [v1, v2].transpose.map(&:sum)
@@ -12,8 +7,8 @@ end
 
 N, S, E, W = [-1, 0], [1, 0], [0, 1], [0, -1]
 NW, NE, SW, SE = vadd(N,W), vadd(N,E), vadd(S,W), vadd(S,E)
-DIR_TO_SYM = {N => :N, S => :S, E => :E, W => :W, NW => :NW, NE => :NE, SW => :SW, SE => :SE}
 ALL_DIRS = [N, S, E, W, NW, NE, SW, SE]
+DIR_TO_SYM = {N => :N, S => :S, E => :E, W => :W, NW => :NW, NE => :NE, SW => :SW, SE => :SE}
 
 require 'set'
 $elves = Set.new
@@ -24,32 +19,13 @@ def borders
     [minx, miny, maxx, maxy]
 end
 
-def print_state
-    minx, miny, maxx, maxy = borders
-    # TODO remove this
-    minx = [-2, minx].min
-    maxx = [maxx, 9].max
-    miny = [-3, miny].min
-    maxy = [maxy, 10].max
-
-    debug "num elves: #{$elves.size}"
-    (minx..maxx).each do |x|
-        debug (miny..maxy).map { |y|
-            $elves.include?([x, y]) ? "#" : "."
-        }.join
-    end
-end
-
 $data.each_with_index do |line, x|
     line.chars.each_with_index do |ch, y|
         $elves.add [x, y] if ch == "#"
     end
 end
 
-debug "=== INITIAL STATE ==="
-print_state
-
-# [directions to check], {direction to move}
+# [[directions to check], {direction to move}]
 $check_move = [
     [[N, NE, NW], N],
     [[S, SE, SW], S],
@@ -57,28 +33,18 @@ $check_move = [
     [[E, NE, SE], E]
 ]
 
-1000000.times do |round|
-    puts "=== ROUND #{round+1} ==="
-    debug "priorities: #{$check_move.map { |_, dir| DIR_TO_SYM[dir] }.join ' > '}"
+1000.times do |round|
     # generate each elf's would-be position
     moves = Hash.new { |h, k| h[k] = [] }
+    conflicts = Hash.new { |h, e| h[e] = ALL_DIRS.select { |dir| $elves.include? vadd(e, dir) } }
     $elves.each do |elf|
-        debug "trying to move elf #{elf}"
-        unless ALL_DIRS.any? { |dir| $elves.include? vadd(elf, dir) }
-            debug "  no elf neighbors. skipping."
-            next
-        end
+        next unless conflicts[elf].any? # no neighbors, skip
 
         $check_move.each do |checks, move|
-            debug "  checking #{checks}"
             npos = vadd(elf, move)
-            conflicts = checks.select { |dir| $elves.include? vadd(elf, dir) }
-            if conflicts.any?
-                debug "  conflicts: #{conflicts.map { |dir| DIR_TO_SYM[dir] }.join ' '}"
-                next
-            end
+            dir_conflicts = conflicts[elf] & checks
+            next if dir_conflicts.any? # try next direction
 
-            debug "  elf #{elf} would move #{DIR_TO_SYM[move]} to #{npos}"
             moves[npos] << elf
             break # go to the next elf
         end
@@ -87,23 +53,26 @@ $check_move = [
     # find the unique moves and apply them
     moves.select! { |_, elves| elves.size == 1 }
     unless moves.any?
+        # part 2
         puts round+1
         break
     end
-
     moves.each do |npos, elves|
-        debug "replacing #{elves.first} with #{npos}"
         $elves.delete elves.first
         $elves.add npos
     end
-    print_state
 
     # rotate the moves
     $check_move.rotate!
+
+
+    if round+1 == 10
+        # part 1
+        minx, miny, maxx, maxy = borders
+        puts (minx..maxx).map { |x|
+            (miny..maxy).map { |y|
+                $elves.include?([x, y]) ? 0 : 1
+            }.sum
+        }.sum
+    end
 end
-#minx, miny, maxx, maxy = borders
-#puts (minx..maxx).map { |x|
-#    (miny..maxy).map { |y|
-#        $elves.include?([x, y]) ? 0 : 1
-#    }.sum
-#}.sum
