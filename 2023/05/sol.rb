fee034536afb9b9089ded86a1b4d6d553e08a780
Chris Kirkland <chriskirkland@github.com>
2023-12-16 13:34:10 -0600

    2023 5b ruby, refactor to use ranges for performance

--- 2023/05/sol.rb
+++ 2023/05/sol.rb
@@ -1,50 +1,73 @@
 # https://adventofcode.com/2022/day/5
 $data = File.read("input.txt").split("\n\n")
-#$data = File.read("sample.txt").split("\n\n")
 
-N = 7
-maps = Array(N) { nil }
+$N = 7
+$MAPS = Array($N) { nil }
 
-p1_seeds = $data[0].scan(/\d+/).map(&:to_i)
+class RangeFunction
+    # collection of dijoint, closed ranges on both input and output domains
+    # { [Range] -> [Range] }
+    def initialize(rangeMap)
+        @map = rangeMap
+    end
 
-#p2_seeds = $data[1].scan(/\d+/).map(&:to_i).each_slice(2) do |start, rng|
-#    (start..start+rng).to_a
-#end.flatten
+    def do(range)
+        drange = partition(range)
+        drange.map { |r| map(r) }
+    end
 
-# [range] -> { disjoint ranges }
-def newMapFunc(mappings) 
-    return lambda do |range|
-        rs = []
-        mappings.each do |src, dst|
-            next unless src.include?(range.begin) || src.include?(range.end)
+    # partitions range based on input ranges in @map. the return ranges in the partition will be covered by some
+    # source range in @map
+    def partition(range)
+        return [range] if range.size == 1
 
+        break_points = @map.map { |r, _| [r.begin, r.end] }.flatten.sort
+        int_points = break_points.select { |p| range.include?(p) }
+        return [range] if int_points.empty?
+
+        new_points = (int_points + [range.begin, range.end]).sort.uniq
+        new_points.each_cons(2).map { |a, b| Range.new(a, b) }
+    end
+
+    # transforms the list of ranges based on the range mappings in @map
+    # [range, ...]
+    def map(range)
+        @map.each do |src, dst|
+            next unless src.cover?(range) 
+
+            offset = range.begin - src.begin
+            nstart = dst.begin + offset
+            return Range.new(nstart, nstart + (range.size - 1))
         end
-        return val
+
+        range
     end
 end
 
 # populate each of the maps (e.g. seed-to-soil)
-0.upto(N-1) do |mx|
+0.upto($N-1) do |mx|
     mappings = $data[mx+1].split("\n").drop(1).map do |line|
         dst, src, len = line.scan(/\d+/).map(&:to_i)
         [Range.new(src, src+len-1), Range.new(dst, dst+len-1)]
     end
-    maps[mx] = newMapFunc(mappings)
+    $MAPS[mx] = RangeFunction.new(mappings)
 end
 
-
-p1_locations = seeds.map do |val|
-    0.upto(N-1) do |mx|
-        val = maps[mx][val]
+def solve(ranges)
+    0.upto($N-1) do |mx|
+        ranges = ranges.map { |r| $MAPS[mx].do(r) }.flatten
     end
-    val
+    ranges
 end
-puts "p1: #{p1_locations.min}"
 
-p2_locations = p2_seeds.map do |val|
-    0.upto(N-1) do |mx|
-        val = maps[mx][val]
-    end
-    val
+p1_seeds = $data[0].scan(/\d+/).map(&:to_i)
+p1_seed_ranges = p1_seeds.sort.map {|val| Range.new(val, val) }
+p1_locations = solve(p1_seed_ranges)
+puts "p1: #{p1_locations.map(&:begin).min}"
+
+
+p2_seed_ranges = $data[0].scan(/\d+/).map(&:to_i).each_slice(2).map do |start, rng|
+    Range.new(start, start+rng-1)
 end
-puts "p2: #{p2_locations.min}"
+p2_locations = solve(p2_seed_ranges)
+puts "p2: #{p2_locations.map(&:begin).min}"
