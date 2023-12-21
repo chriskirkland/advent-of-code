a1d22bd7dc8639821be159ab18a1bea41685c89d
Chris Kirkland <chriskirkland@github.com>
2023-12-21 14:04:05 -0600

    ughhhhhhh 10b

--- 2023/10/sol.rb
+++ 2023/10/sol.rb
@@ -1,6 +1,7 @@
 # https://adventofcode.com/2022/day/10
 #$data = File.read("input.txt").split("\n")
-$data = File.read("sample.txt").split("\n")
+#$data = File.read("sample.txt").split("\n")
+$data = File.read("sample2.txt").split("\n")
 
 $MOVE = {
     DOWN: [1, 0],
@@ -58,7 +59,7 @@ def solve(start)
     # [position, from_direction, path]
     paths = $MOVE.map do |dir, move|
         nv = vadd(start, move)
-        [nv, dir, [start, nv]]
+        [nv, dir, [[start, dir]]]
     end
 
     until paths.empty?
@@ -71,9 +72,10 @@ def solve(start)
         next unless ndir # dead end
         nx, ny = vadd(curr, $MOVE[ndir])
 
-        return path if [nx, ny] == start # finished the loop
+        npath = path + [[[cx,cy], dir]] # extend path
+        return npath if [nx, ny] == start # finished the loop
 
-        paths.append [[nx, ny], ndir, path + [[nx, ny]]] # extend path
+        paths.append [[nx, ny], ndir, npath]
     end
     raise "No pipe loop found"
 end
@@ -85,42 +87,89 @@ S = COORDS.find { |i, j| $grid[i][j] == "S"}
 cycle = solve(S)
 puts "p1: #{(cycle.size+1)/2}"
 
-def dist_to_edge(i, j)
-  [i, N-1-i, j, M-1-j].min
+# replace S with necessary part base on start + end direction of loop
+newS = case [cycle[0][1], cycle[-1][1]]
+when [:DOWN, :LEFT], [:RIGHT, :UP]
+  "F"
+when [:DOWN, :RIGHT], [:LEFT, :UP]
+  "7"
+when [:UP, :LEFT], [:RIGHT, :DOWN]
+  "L"
+when [:UP, :RIGHT], [:LEFT, :DOWN]
+  "J"
+when [:LEFT, :RIGHT], [:RIGHT, :LEFT]
+  "-"
+when [:UP, :DOWN], [:DOWN, :UP]
+  "|"
+else
+  raise "INVALID"
 end
+puts "cycle ends: #{[cycle[0], cycle[-1]]} => #{newS} @ #{S}"
+$grid[S[0]][S[1]] = newS
 
 
-# rings of given distance from the edge (min distance among both coordinates is EXACTLY $dist)
-def ring(dist, &block)
-    raise "no block" unless block_given?
+### PART 2
 
-    COORDS.each do |i, j|
-        next unless dist_to_edge(i, j) == dist
-        yield [i, j]
+$scaled = Array.new(2*N) { Array.new (2*M) { '.' } }
+# copy in a 2X scaled up version fo the $grid
+COORDS.each { |i,j| $scaled[2*i][2*j] = $grid[i][j] }
+
+# fill in all connected segments of pipe as we scale up, path as well
+path = cycle.map { |node| node[0] }.sort
+spath = path.map { |i, j| [2*i,2*j] }
+
+puts "cycle: #{cycle.sort}"
+puts "path: #{path}"
+COORDS.each do |i,j|
+  puts "  ? (#{i},#{j})"
+  if i < N - 1
+    # look down the column
+      if ["F", "7", "|"].include?($scaled[2*i][2*j]) && ["L", "J", "|"].include?($scaled[2*(i+1)][2*j])
+        if path.include?([i,j]) && path.include?([i+1,j])
+          puts "adding (#{2*i+1},#{2*j}) to path"
+          spath.append([2*i+1,2*j])
+        end
+        $scaled[2*i+1][2*j] = "|"
+      end
+  end
+
+  if j < M - 1
+    # look across the row
+    if ["F", "L", "-"].include?($scaled[2*i][2*j]) && ["7", "J", "-"].include?($scaled[2*i][2*(j+1)])
+      if path.include?([i,j]) && path.include?([i,j+1])
+        puts "adding (#{2*i},#{2*j+1}) to path"
+        spath.append([2*i,2*j+1])
+      end
+      $scaled[2*i][2*j+1] = "-"
     end
+  end
 end
+puts "path length #{path.size}"
+puts "scaled path length #{spath.size}"
+puts "spath: #{spath.sort}"
 
-# pipe piece orthogonal to the direction of travel (i.e. stops a path to outside)
-ORTHO = {
-    RIGHT: "|",
-    LEFT: "|",
-    UP: "-",
-    DOWN: "-",
-}
-
-# '.' points outside the loop
-$outside = {}
-ring(0) do |i, j|
-    # draw an extra outside boundary
-    $outside[[i,j]] = true
+# fill in the path as well
+#
+puts ">>> SCALED UP"
+$scaled.each do |line|
+  puts line.join
 end
-puts "outside: #{$outside}"
+puts
 
-def print_outside
-    $grid.each_with_index do |row, i|
+
+# all points not yet reached by flooding
+$inside = (0..2*N-1).to_a.product((0..2*M-1).to_a).select { |i,j| !spath.include?([i,j]) }
+
+def print_grid
+    $scaled.each_with_index do |row, i|
         nrow = row.map.with_index do |c, j|
             if c == "."
-                $outside[[i,j]] ? "O" : "I"
+              $inside.include?([i,j]) ? "I" : "O"
+              #if i%2 == 1 || j%2 == 1
+              #  " "
+              #else
+              #  $inside.include?([i,j]) ? "I" : "O"
+              #end
             else
               c
             end
@@ -130,40 +179,32 @@ def print_outside
     puts
 end
 
-
-# traverse rings from outside to inside, repeating until nothing changes
-pass = 1
-begin
-  puts "****** PASS # #{pass} ******"
-  changed = false
-  min_outside = COORDS.map {|i, j| dist_to_edge(i, j) if $outside[[i,j]]}.compact.min
-
-  (min_outside..[N,M].min/2).each do |off|
-      puts
-      puts "#### RING #{off}"
-      print_outside
-
-      ring(off) do |i, j|
-          next if $outside[[i,j]] # no need to check neighbors
-          puts "[r#{off}] '#{$grid[i][j]}' @ (#{i},#{j})"
-
-          neigbors_outside = $MOVE.map do |dir, move|
-              ni, nj = vadd([i, j], move)
-              # neighbor is outside and doesn't block the path outside in the given direction
-              outside = $outside[[ni,nj]] && $grid[ni][nj] != ORTHO[dir]
-              puts "  neighbor '#{$grid[ni][nj]} @ (#{ni},#{nj}) outside? #{outside}"
-              outside
-          end
-          if neigbors_outside.any?
-            $outside[[i,j]] = true
-            changed = true
-          end
-      end
-  end
-  pass += 1
-
-end until !changed
-
+puts "----- BEFORE ----"
+print_grid
 puts
-puts "#### FINAL"
-print_outside
+
+Q = [[0, 0]]
+$inside.delete [0,0]
+
+until Q.empty?
+  pi, pj = Q.shift
+  puts ">> visiting '#{$scaled[pi][pj]}' @ (#{pi},#{pj})"
+  print_grid
+
+  # enqueue all valid directions from pt
+  [:UP, :DOWN, :LEFT, :RIGHT].each do |dir|
+    ni, nj = vadd([pi, pj], $MOVE[dir])
+    next unless $inside.include? [ni, nj]  # already visited or on the path
+
+    puts " + '#{$scaled[ni][nj]}' @ (#{ni},#{nj})"
+    $inside.delete [ni, nj] # point is reachable from outside
+    Q.append [ni,nj]
+  end
+end
+
+puts "----- AFTER ----"
+print_grid
+puts
+
+normal_inside = $inside.select { |i,j| i%2 == 0 && j%2 == 0 }
+puts  "p2: #{normal_inside.size}"
